42d88cbfe724fb30801f0734343a066e17a3138c
uaa490 <marc@chezbrown.org>
2022-09-15 19:47:01 -0800

    first commit

---
+++ classtoday/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+import datetime
+
+# Create your views here.
+def index(request):
+    now = datetime.datetime.now()
+    isClasstoday = now.weekday() == 1 or now.weekday() == 3
+    s = "YES" if isClasstoday else "NO"
+    return render(request, "classtoday/mainpage.html", {"classtoday": isClasstoday})
